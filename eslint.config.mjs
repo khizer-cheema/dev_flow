4e10817d43aa72f114aecd9f839baf75dcd50002
khizer hayat <khizer.cheema.pas@gmail.com>
2025-05-22 18:51:02 +0500

    Impement eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,73 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  // Ignore specific folders
+  {
+    ignores: ["components/ui/**/*"],
+  },
+
+  // Use standard, Next.js, Prettier, Tailwind configs
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    //"plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+
+  // General custom rules for all JS/TS files
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "comma-dangle": "off",
+    },
+  },
+
+  // TypeScript-specific overrides
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-undef": "off",
+    },
+  },
 ];
 
-export default eslintConfig;
+export default config;
