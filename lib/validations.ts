9db45bddc82661aad6c3dce3ac8ada5aa606d662
khizer hayat <khizer.cheema.pas@gmail.com>
2025-06-29 12:04:45 +0500

    implement tagQuestions action

--- lib/validations.ts
+++ lib/validations.ts
@@ -141,3 +141,7 @@ export const paginatedSearchParamsSchema =z.object({
   filter:z.string().optional(),
   sort:z.string().optional()
 })
+
+export const getTagQuestionsSchema = paginatedSearchParamsSchema.extend({
+  tagId:z.string().min(1,{message:"Tag ID is required"})
+})
--- types/action.d.ts
+++ types/action.d.ts
@@ -27,3 +27,6 @@ interface EditQuestionParams extends CreateQuestionParams {
 interface GetQuestionParams {
     questionId:string;
 }
+interface getTagQuestionsParams extends Omit<paginatedSearchParams,"filter"> {
+  tagId:string
+}
